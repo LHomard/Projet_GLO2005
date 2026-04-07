c6e61f56b9a2457deac28b8086a1cdfc01b596ed
Émile Marcotte <timile27@gmail.com>
2026-04-07 14:38:44 -0400

    docker start + pull changes from feature home

--- src/server/run.py
+++ src/server/run.py
@@ -1,9 +1,23 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-from app import create_app
+import os
+import pymysql
+from src.server.app import create_app
 
 app = create_app()
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
+@app.route('/')
+def home():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT DATABASE();")
+    conn.close()
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
