5fbba8e492957d60b01ff2dbf564792ab4477c38
Alexandre Khuong <alexandre.khuong@gmail.com>
2026-04-11 02:09:40 -0400

    feature: fixed import player from csv function

---
+++ backend/app/Db_queries/import_player_from_csv.py
@@ -0,0 +1,29 @@
+import csv
+from datetime import date
+
+from backend.app.Db_queries.login_queries import get_connection, hash_password
+
+
+def import_players_from_csv(csv_file):
+    connection = get_connection()
+
+    try:
+        with open(csv_file, newline='') as file:
+            render = csv.DictReader(file)
+
+            for row in render:
+                username = row['username']
+                email = row['email']
+                password = row['password']
+
+                hashed_password = hash_password(password)
+
+                request = """INSERT IGNORE INTO Players (register_date, username, email, password_hash) VALUES ('{}', '{}', '{}', '{}')""".format(
+                date.today(), username, email, hashed_password)
+
+        print("All players insert successfully!")
+
+    finally:
+        connection.close()
+
+import_players_from_csv('data/players.csv')
