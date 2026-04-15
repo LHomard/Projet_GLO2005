1daab867280f28835cbddcc553f97cba8a466930
Alexandre Khuong <alexandre.khuong@gmail.com>
2026-04-14 20:12:47 -0400

    feature: Indices for important querries and comments

--- table.sql
+++ table.sql
@@ -104,6 +104,13 @@ CREATE TABLE IF NOT EXISTS Deck_composition (
     PRIMARY KEY (id_deck, id_printing)
 );
 
+-- Index permettant d'accélérer les requêtes fréquentes
+CREATE INDEX idx_decks_users ON Decks(id_user);
+CREATE INDEX idx_ai_chats_player ON Ai_chats(id_player);
+CREATE INDEX idx_card_printing_oracle ON Card_printing(id_oracle);
+CREATE INDEX idx_card_printing_set ON Card_printing(id_set);
+CREATE INDEX idx_card_printing_rarity_price ON Card_printing(rarity, price);
+
 
 -- Trigger permettant de vérifier l'âge ainsi que la validité du email lors de la création d'un player
 DELIMITER $$
